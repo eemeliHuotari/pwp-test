4cfc7877c27dcab1554f116dd233dd07195ef2ce
epelix <90451715+eemeliHuotari@users.noreply.github.com>
2025-04-25 13:07:16 +0300

    Update settings.py

--- burgir/settings.py
+++ burgir/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0v#u(k)55$g%0@0%vws!0^y7@q^(2=q799e%vs^gh37ij-^z%y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 SECURE_SSL_REDIRECT = True
-ALLOWED_HOSTS = ['burgir.2.rahtiapp.fi', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['burgirs.2.rahtiapp.fi', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -141,13 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 S# Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# For production, you might also want:
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_URL = '/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
