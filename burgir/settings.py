7ba87b5cbcdabf72edf6188d4f9daed6f04239c9
epelix <90451715+eemeliHuotari@users.noreply.github.com>
2025-04-25 13:08:55 +0300

    Update settings.py

--- burgir/settings.py
+++ burgir/settings.py
@@ -140,8 +140,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-S# Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
