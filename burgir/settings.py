d35eff9436b361e85b23a81419f71f77cf6cada8
epelix <90451715+eemeliHuotari@users.noreply.github.com>
2025-04-30 14:07:12 +0300

    Update settings.py

--- burgir/settings.py
+++ burgir/settings.py
@@ -57,10 +57,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_spectacular',
+    'corsheaders',
     "app"
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -157,7 +159,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+CORS_ALLOWED_ORIGINS = [
+    "https://burgirsclient.2.rahtiapp.fi",
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
